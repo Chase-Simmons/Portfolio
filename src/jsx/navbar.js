5b17cee28f59446e9ece19834d25bdf9ee04981a
Chase Simmons <csim6890@gmail.com>
2021-05-02 00:56:16 -0500

    first commit

---
+++ src/jsx/navbar.js
@@ -0,0 +1,15 @@
+export default function navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-brand"></div>
+        <ul className="navbar-nav">
+          <li className="nav-item">Home</li>
+          <li className="nav-item">Works</li>
+          <li className="nav-item">About</li>
+          <li className="nav-item">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ src/raw.js
@@ -0,0 +1,1 @@
+console.log('raw js loaded');
