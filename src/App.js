bdbb0cb1f8da27105f3e8acd3cd31090f3ca55da
Chase Simmons <csim6890@gmail.com>
2021-05-02 21:37:19 -0500

    added home

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/navbar/navbar';
 import Particles from './components/particles/particles';
+import Home from './components/home/home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
-        <Particles />
+        <div className="landing">
+          {/* <Particles /> */}
+          <Home />
+          <div className="fix-particle-margin"></div>
+        </div>
       </div>
     );
   }
---
+++ src/components/home/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <h1>My name is Chase.</h1>
+        <h1>Software Engineer</h1>
+      </div>
+    );
+  }
+}
