7e015e133955301905c52e3550cb6f758915b362
Chase Simmons <csim6890@gmail.com>
2021-05-02 21:13:03 -0500

    added particles fx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/navbar/navbar';
+import Particles from './components/particles/particles';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
+        <Particles />
       </div>
     );
   }
---
+++ src/components/particles/particles.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Particles from 'particlesjs';
+
+import './particles.css';
+
+window.onload = function () {
+  Particles.init({
+    selector: '.particles',
+    maxParticles: 150,
+    speed: 0.4,
+    color: '#FEFEFE',
+    minDistance: 105,
+    connectParticles: true,
+  });
+};
+
+export default class ParticlesComponent extends Component {
+  render() {
+    return <canvas className="particles"></canvas>;
+  }
+}
