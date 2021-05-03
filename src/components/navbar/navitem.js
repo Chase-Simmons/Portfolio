9941f380d3770f93c1cda005093c2698cf2f0d9a
Chase Simmons <csim6890@gmail.com>
2021-05-03 04:05:52 -0500

    code cleanup more better mapping

--- src/components/navbar/navitem.js
+++ src/components/navbar/navitem.js
@@ -4,12 +4,12 @@ import highlight from './navhighlight';
 let isTransitioning;
 let oldLocation;
 
-const eachNode = {};
+const Nodes = {};
 
 export default class NavItem extends Component {
   componentDidMount() {
     const testNode = this._reactInternals.child.stateNode;
-    eachNode[testNode.innerHTML] = testNode;
+    Nodes[testNode.innerHTML] = testNode;
     if (testNode.innerHTML !== 'Home') return;
     highlight.style.width = `${testNode.clientWidth}px`;
     highlight.style.height = `${testNode.clientHeight}px`;
@@ -19,7 +19,11 @@ export default class NavItem extends Component {
   }
   render() {
     window.addEventListener('resize', () => {
-      highlight.style.cssText = `transform: translate(${eachNode.Home.offsetLeft}px, ${eachNode.Home.offsetTop}px); width: ${eachNode.Home.clientWidth}px; height: ${eachNode.Home.clientHeight}px; `;
+      highlight.style.cssText = `transform: translate(${
+        Nodes.Home.offsetLeft
+      }px, ${Nodes.Home.offsetTop - 1}px); width: ${
+        Nodes.Home.clientWidth
+      }px; height: ${Nodes.Home.clientHeight}px; `;
     });
 
     const props = this.props;
@@ -40,16 +44,7 @@ export default class NavItem extends Component {
     }
 
     function onMouseEnter(e) {
-      if (!e.relatedTarget.childNodes) return;
-
-      const nodeChildren = [...e.relatedTarget.childNodes];
-
-      if (nodeChildren.length !== 4) return;
-
-      const thisNode = nodeChildren.filter(
-        (node) => node.innerHTML === props.name
-      );
-
+      const thisNode = Nodes[props.name];
       // console.log(nodeChildren.filter((node) => node.innerHTML === props.name));
 
       if (isTransitioning !== true) {
@@ -58,10 +53,10 @@ export default class NavItem extends Component {
       }
 
       const coords = {
-        width: thisNode[0].clientWidth,
-        height: thisNode[0].clientHeight,
-        top: thisNode[0].offsetTop - 1,
-        left: thisNode[0].offsetLeft,
+        width: Nodes[props.name].clientWidth,
+        height: Nodes[props.name].clientHeight,
+        top: Nodes[props.name].offsetTop - 1,
+        left: Nodes[props.name].offsetLeft,
       };
 
       highlight.style.width = `${coords.width}px`;
