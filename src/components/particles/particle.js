8fc5b59726168f0fc3ce9dd2cc14f99360f006aa
Chase Simmons <csim6890@gmail.com>
2021-05-03 19:27:26 -0500

    custom particles properly transition needs optimization for all devices

--- src/components/particles/particle.js
+++ src/components/particles/particle.js
@@ -5,13 +5,13 @@ let totalParticles = 0;
 const config = {
   speed: 10,
   radius: 6,
-  amount: 10,
+  amount: 40,
 };
 
 function buildParticle() {
   const particle = document.createElement('span');
 
-  particle.style.transition = `all ${config.speed}s`;
+  particle.style.transition = `all ${config.speed}s linear`;
   particle.style.width = `${config.radius}px`;
   particle.style.height = `${config.radius}px`;
   particle.classList.add('particle');
@@ -21,11 +21,26 @@ function buildParticle() {
       particle
     );
 
-    particle.style.transform = `translate(2vw, 5vh)`;
+    particle.style.transform = `translate(53.7vw, 19.85vh)`;
 
-    setTimeout(() => {
-      particle.style.transform = `translate(89vw, 27vh)`;
-    }, 1000);
+    function translate() {
+      setTimeout(() => {
+        particle.style.transform = `translate(96.5vw, 24.7vh)`;
+        setTimeout(() => {
+          particle.style.transform = `translate(93.3vw, 72.5vh)`;
+          setTimeout(() => {
+            particle.style.transform = `translate(50.2vw, 67.7vh)`;
+            setTimeout(() => {
+              particle.style.transform = `translate(53.7vw, 19.85vh)`;
+              setTimeout(() => {
+                translate();
+              }, 1000 * config.speed - 100);
+            }, 1000 * config.speed);
+          }, 1000 * config.speed);
+        }, 1000 * config.speed);
+      }, 100);
+    }
+    translate();
 
     console.log(allParticles);
     totalParticles++;
