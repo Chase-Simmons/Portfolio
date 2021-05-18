08c3285906a40fb05b2bde98b7d2df1e5255a650
Chase Simmons <csim6890@gmail.com>
2021-05-18 00:26:43 -0500

    added details about works

--- src/components/works/worksdetails.js
+++ src/components/works/worksdetails.js
@@ -1,8 +1,18 @@
 module.exports = [
   {
+    title: 'ConnectHer',
+    description: `InnovateHER KC, a nonprofit in Kansas City, MO committed to supporting all womxn, promoting diversity, and advancing its member’s professional goals—came to Prime Digital Academy with a challenge.
+      Their online presence began on Facebook and has grown to 3,000+ members. As the community has grown, so have its needs. For example, while the group was smaller and the FB Page had less activity, sifting through previous posts for like-minded people and resources wasn’t as inconvenient as it is now. Today, the page is so active that finding a post from an hour ago is a challenge.
+      In summary, the ask was to conveniently organize InnovateHER’s resources for member’s easy access and provide a more targeted way of connecting with similar and/or complementary individuals.
+      Out of this request, ConnectHER, a social networking application tailored to InnovateHER, emerged. It promotes inclusive interaction while also aligning specific members based on similarities, gathers analytics in a straightforward manner allowing newfound knowledge to better the user experience, and ultimately, makes community resources more easily accessible.`,
+  },
+  {
     title: 'Necromancy Online',
     description: 'This is Necromancy Online',
   },
-  { title: 'Wi-Find', description: 'This is Wi-Find' },
-  { title: 'ConnectHer', description: 'This is ConnectHer' },
+  {
+    title: 'Wi-Find',
+    description:
+      'This is Wi-Wi-Find is Free Public Wi-Fi locator application. With the increase of remote work working from home every single day can begin to come dry, and the easiest way to change up the scenery is by heading to a local coffee shop.',
+  },
 ];
